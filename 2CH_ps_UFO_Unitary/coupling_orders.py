8dc1c3a0dac531167818e64fb9aa4f4c7fc35e85
Lara Mason <lara.mason@live.com>
2019-04-29 15:28:00 +0200

    fixed ferm vertices to not let sigma change flavours

---
+++ 2CH_ps_UFO_Unitary/coupling_orders.py
@@ -0,0 +1,76 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 11.3.0 for Mac OS X x86 (64-bit) (March 7, 2018)
+# Date: Mon 29 Apr 2019 13:00:47
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+NoBlockName[FRBlock] = CouplingOrder(name = 'NoBlockName[FRBlock]',
+                                     expansion_order = 99,
+                                     hierarchy = 1)
+
+Gamma5[1, 3] -> 1 = CouplingOrder(name = 'Gamma5[1, 3] -> 1',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[1, 4] -> 0 = CouplingOrder(name = 'Gamma5[1, 4] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[2, 1] -> 0 = CouplingOrder(name = 'Gamma5[2, 1] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[2, 2] -> 0 = CouplingOrder(name = 'Gamma5[2, 2] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[2, 3] -> 0 = CouplingOrder(name = 'Gamma5[2, 3] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[2, 4] -> 1 = CouplingOrder(name = 'Gamma5[2, 4] -> 1',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[3, 1] -> 1 = CouplingOrder(name = 'Gamma5[3, 1] -> 1',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[3, 2] -> 0 = CouplingOrder(name = 'Gamma5[3, 2] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[3, 3] -> 0 = CouplingOrder(name = 'Gamma5[3, 3] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[3, 4] -> 0 = CouplingOrder(name = 'Gamma5[3, 4] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[4, 1] -> 0 = CouplingOrder(name = 'Gamma5[4, 1] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[4, 2] -> 1 = CouplingOrder(name = 'Gamma5[4, 2] -> 1',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[4, 3] -> 0 = CouplingOrder(name = 'Gamma5[4, 3] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
+Gamma5[4, 4] -> 0 = CouplingOrder(name = 'Gamma5[4, 4] -> 0',
+                                  expansion_order = 99,
+                                  hierarchy = 1)
+
